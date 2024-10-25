e9c1bc4dbea4c868568dd9707c22a884ff90c544
josephg <9719@holbertonstudents.com>
2024-10-25 10:27:57 -0400

    Redux

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,13 @@ int main(void)
 	int i = 97;
 
 	while (i <= 122)
-	if (i != 113 && i != 101)
-	i++;
 	{
-	putchar(i);
+	if (i != 113 && i != 101)
+		{
+		putchar(i);
+		i++;
+		}
 	}
 	putchar('\n');
 	return (0);
-
 }
