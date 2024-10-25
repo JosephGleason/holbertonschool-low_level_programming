03d59d3a05bf72c25febc095055699bedbc5b8b6
josephg <9719@holbertonstudents.com>
2024-10-25 09:59:15 -0400

    rogram that prints the alphabet in lowercase, followed by a new line.

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: (0)
+ *
+ **/
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	if (i != 113 && i != 101)
+	i++;
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
