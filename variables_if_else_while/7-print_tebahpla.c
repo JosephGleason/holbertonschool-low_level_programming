cd588b59ef2f38e7e895da4265160e1700dcbed2
josephg <9719@holbertonstudents.com>
2024-10-25 13:06:32 -0400

    corrected output. added >= instead of <=

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: (0)
+ *
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+
+}
