dfb885886f353426b7f1632bfcaf4e31b9381140
JosephGleason <9719@holbertonstudents.com>
2024-11-15 15:55:39 -0400

    function that prints the chessboard.

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: array a being evaluated
+ */
+void print_chessboard(char (*a)[8])
+{
+    int rows, columns;
+
+    for (rows = 0; rows < 8; rows++)
+    {
+        for (columns = 0; columns < 8; columns++)
+        {
+            _putchar(a[rows][columns]);
+        }
+        _putchar('\n');
+    }
+}
+
