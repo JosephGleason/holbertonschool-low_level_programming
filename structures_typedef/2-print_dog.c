fbcbe88ccfbd3d7e54a4f95a36c5d3908c2d3629
josephg <9719@holbertonstudents.com>
2024-11-27 15:56:30 -0400

    corrected formating

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,23 +13,29 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	else if (d->name == NULL)
+	if (d->name == NULL)
 	{
 		printf("Name: (nil)");
 	}
-	else if (d->age < 0)
-	{
-		printf("Age: (nil)");
-	}
-	else if (d->owner == NULL)
-	{
-		printf("Owner: (nil)");
-	}
-
 	else
 	{
 		printf("Name: %s\n", d->name);
+	}
+	if (d->age < 0)
+	{
+		printf("Age: (nil)");
+	}
+	else
+	{
 		printf("Age: %f\n", d->age);
+	}
+	
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+	else
+	{
 		printf("Owner: %s\n", d->owner);
 	}
 }
