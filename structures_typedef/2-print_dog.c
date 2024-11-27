ec1460f49e47d871c2253f93c72ed0f5148ca9f1
josephg <9719@holbertonstudents.com>
2024-11-27 15:37:13 -0400

    added unction that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints the details of a struct dog
+ * @d: Pointer to the struct dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	else if (d->name == NULL)
+	{
+		printf("(nil)");
+	}
+	else if (d->age == 0)
+	{
+		printf("(nil)");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("(nil)");
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
