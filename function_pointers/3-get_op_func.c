60c0c15073c958b5e3b97c805708eee31aad9549
josephg <9719@holbertonstudents.com>
2024-12-06 12:58:31 -0400

    corrected summation

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,11 +20,13 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while ( ops[i].op != NULL)
+	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (*ops[i].op == *s)
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
