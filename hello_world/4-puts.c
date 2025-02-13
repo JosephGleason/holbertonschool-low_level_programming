ef354ce6e8ef234139ad9157dbbe1dacac80b277
josephg <9719@holbertonstudents.com>
2025-02-13 10:54:47 -0400

    fixed code and added description

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
