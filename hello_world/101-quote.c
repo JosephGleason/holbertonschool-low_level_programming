12a44772f64fbdd6615cf03953fb1306a4b6e1e3
josephg <9719@holbertonstudents.com>
2025-02-13 12:41:10 -0400

    another way

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,15 +2,18 @@
  * main - Prints a quote to standard error
  *
  * Description:
- *   The main function prints a quote to the standard error using the write
- *   system call and returns 1 to indicate an error status.
+ *   The main function prints a quote to the standard error using the fwrite
+ *   function and returns 1 to indicate an error status.
+ *
+ *   The quote printed is:
+ *   "and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
  * Return: 1 (error status)
  */
-#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+    return (1);
 }
