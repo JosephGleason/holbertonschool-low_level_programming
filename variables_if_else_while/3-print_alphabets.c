ba38d0b0563705b0b36a8f04b6bb4e3235ddec52
josephg <9719@holbertonstudents.com>
2025-02-19 11:03:01 -0400

    fixed and reuploaded

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,20 @@
  **/
 int main(void)
 {
-        char letters = 97;
+	char letters = 97;
 	char LETTERS = 65;
 
-        while (letters < 123)
-        {
-                putchar(letters);
-                letters++;
-        }
+	while (letters < 123)
+	{
+		putchar(letters);
+		letters++;
+	}
 
 	while (LETTERS < 91)
 	{
 		putchar(LETTERS);
 		LETTERS++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
