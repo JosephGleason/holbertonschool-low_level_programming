f48581940c97296e73af2bdb192252fb24bd9382
josephg <9719@holbertonstudents.com>
2025-02-19 10:34:03 -0400

    fixed 1.the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,15 +24,15 @@ int main(void)
 
 	if (digit > 5)
 	{
-		printf("and is greater than 5\n");
+		printf(" and is greater than 5\n");
 	}
 	else if (digit == 0)
 	{
-		printf("and is 0\n");
+		printf(" and is 0\n");
 	}
-	else 
+	else
 	{
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	return (0);
 }
