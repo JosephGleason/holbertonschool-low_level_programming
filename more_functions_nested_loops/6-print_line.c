bad775d8f597a1625ac7189f305be856a41f7d6a
josephg <9719@holbertonstudents.com>
2025-02-25 10:37:29 -0400

    fixed newline error. lets see

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,13 @@ void print_line(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (i = 0; i < n; i++)
 	{
-		if (i <= 0)
-		{
-			_putchar('\n');
-		}
 		_putchar('_');
 	}
 	_putchar('\n');
