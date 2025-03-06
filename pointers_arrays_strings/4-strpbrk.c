6e7cf398e2e95d48b697df187d7c809c8099a416
josephg <9719@holbertonstudents.com>
2025-03-06 13:45:54 -0400

    added unction that locates a substring.

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string being evaluated
+ * @accept: bytes being searched for
+ *
+ * Return: pointer to the byte in a or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
