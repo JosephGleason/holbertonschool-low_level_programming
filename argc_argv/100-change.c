152c651bd2f0901b8dd8be61e07a3a3340e78436
JosephGleason <9719@holbertonstudents.com>
2025-03-13 14:26:22 -0400

    betty

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -6,26 +6,20 @@
  * @argc: Number of command-line arguments.
  * @argv: Array of pointers to the arguments.
  *
- * Return: 0 on success, 1 if the number of arguments is incorrect.
+ * Return: 0 on success, 1 on error.
  */
-int main(argc, argv)
-	int argc;
-	char *argv[];
+int main(int argc, char *argv[])
 {
 	int cents, i, coins;
 	int coin_values[5] = {25, 10, 5, 2, 1};
 
-	/* Check for exactly one argument (excluding program name) */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	/* Convert argument to integer using atoi */
 	cents = atoi(argv[1]);
-
-	/* If the number is negative, print 0 and exit */
 	if (cents < 0)
 	{
 		printf("0\n");
@@ -33,14 +27,11 @@ int main(argc, argv)
 	}
 
 	coins = 0;
-	/* For each coin value, determine how many coins of that value are needed */
 	for (i = 0; i < 5; i++)
 	{
 		coins += cents / coin_values[i];
 		cents %= coin_values[i];
 	}
-
-	/* Print the minimum number of coins needed, followed by a new line */
 	printf("%d\n", coins);
 	return (0);
 }
