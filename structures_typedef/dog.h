5b18faeb15a8240e06804a6bef47266f247c3750
josephg <9719@holbertonstudents.com>
2025-03-14 14:55:23 -0400

    defined a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a string containing the dog's name
+ * @age: Floating point number representing the dog's age
+ * @owner: Pointer to a string containing the owner's name
+ *
+ * Description: A structure that stores details about a dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
