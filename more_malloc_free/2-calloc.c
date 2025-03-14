53de76984c3b93c079da0686337cfe3a02d7c205
josephg <9719@holbertonstudents.com>
2025-03-14 14:39:25 -0400

    added function that allocates memory for an array, using malloc.

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,49 +1,33 @@
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings, using at most n bytes from s2.
- * @s1: First string.
- * @s2: Second string.
- * @n: Number of bytes from s2 to concatenate.
+ * _calloc - allocates memory for an array
  *
- * Return: Pointer to the newly allocated concatenated string, or NULL if it fails.
+ * @nmemb: the number of elements
+ * @size: the size of that element type
+ *
+ * Return: A pointer to the allocated memory. NULL if nmemb or size is 0.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, j, len1 = 0, len2 = 0;
-	char *concat;
+	unsigned int i;
+	char *one;
 
-	/* Treat NULL as empty string */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	/* Get lengths of s1 and s2 */
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-
-	/* Use full s2 if n >= len2 */
-	if (n > len2)
-		n = len2;
-
-	/* Allocate memory for concatenated string (+1 for '\0') */
-	concat = malloc((len1 + n + 1) * sizeof(char));
-	if (concat == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	/* Copy s1 into concat */
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
+	one = malloc(size * nmemb);
 
-	/* Copy first n bytes of s2 into concat */
-	for (j = 0; j < n; j++, i++)
-		concat[i] = s2[j];
+	if (one == NULL)
+		return (NULL);
 
-	/* Null-terminate the new string */
-	concat[i] = '\0';
+	/* Initialize all bytes to 0 */
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		one[i] = 0;
+	}
 
-	return (concat);
+	return (one);
 }
