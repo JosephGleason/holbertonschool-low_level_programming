ab1005d3b7f7311a855b10bad79cef9856bb0ed6
JosephGleason <9719@holbertonstudents.com>
2025-04-06 14:31:00 -0400

    fixed return value

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	bytes_written = write(fd, text_content, strlen(text_content));
