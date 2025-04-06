3386ee6fee5f85f660c452b8380327fd62a5182d
JosephGleason <9719@holbertonstudents.com>
2025-04-06 14:01:25 -0400

    fixed betty and compiled

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,27 +12,27 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = malloc(letters);
-	int fd = open(filename, O_RDONLY);
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
-	if (fd == -1)
-	{
-		perror("Error opening file");
+	if (filename == NULL)
 		return (0);
-	}
 
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		perror("Error allocating memory");
 		close(fd);
 		return (0);
 	}
 
-	ssize_t bytes_read = read(fd, buffer, letters);
-
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
-		perror("Error reading file");
 		free(buffer);
 		close(fd);
 		return (0);
@@ -41,14 +41,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		perror("Error writing to stdout");
 		free(buffer);
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
-
 	return (bytes_written);
 }
+
