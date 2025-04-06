19b776a62fe2283be96963fa9a6d9bac5955c57e
JosephGleason <9719@holbertonstudents.com>
2025-04-06 14:10:14 -0400

    added  function that creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file and writes content into it.
+ * @filename: Name of the file to create.
+ * @text_content: Content to write into the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (-1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <string.h>
 
 int _putchar(char c);
 
